2bd9c1ca3e311ff7fbf9138101aa48b3362fc875
itai rozen <itai.rozen@mail.huji.ac.il>
2020-11-08 20:05:13 +0200

    app.get link

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ app.use(express.urlencoded({extends: true}))
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  app.get('/api', (req,res)=> {
+  app.get('/', (req,res)=> {
     Preset.find()
     .sort({createdAt: -1})
     .then(result => res.json(result))
     .catch(err => console.error(err))
 })
 
-app.post('/api', (req,res)=> {
+app.post('/', (req,res)=> {
     const preset = new Preset(req.body)
     console.log(req.body)
     preset.save()
