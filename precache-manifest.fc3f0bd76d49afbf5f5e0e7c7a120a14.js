6341ecd70ada4ca2745172e05a0690cfa691842b
itai rozen <itai.rozen@mail.huji.ac.il>
2020-11-08 18:52:28 +0200

    Updates

---
+++ precache-manifest.fc3f0bd76d49afbf5f5e0e7c7a120a14.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5944bc04f472ffd267c963a2e636ef57",
+    "url": "/beatmaker/index.html"
+  },
+  {
+    "revision": "03552a88130b996e59d6",
+    "url": "/beatmaker/static/css/main.33fe5462.chunk.css"
+  },
+  {
+    "revision": "2c38c182b23eead1d6b0",
+    "url": "/beatmaker/static/js/2.16738a3d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/beatmaker/static/js/2.16738a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffee5bfd806b7bba91a",
+    "url": "/beatmaker/static/js/3.34ffda33.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/beatmaker/static/js/3.34ffda33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03552a88130b996e59d6",
+    "url": "/beatmaker/static/js/main.84b9374a.chunk.js"
+  },
+  {
+    "revision": "f48dc7459481fc1ede3f",
+    "url": "/beatmaker/static/js/runtime-main.578b88bd.js"
+  },
+  {
+    "revision": "0f3080b9498ab5bc14e6c13570313ba4",
+    "url": "/beatmaker/static/media/2.0f3080b9.jpg"
+  },
+  {
+    "revision": "3f94a69eeceb496fd0fc9031e137af84",
+    "url": "/beatmaker/static/media/all-instruments.3f94a69e.mp3"
+  },
+  {
+    "revision": "4c92d2fd76b642deb8c448bf2269ea08",
+    "url": "/beatmaker/static/media/punk kid.4c92d2fd.ttf"
+  },
+  {
+    "revision": "eb93a08ade6de32e954ee135cd390b4f",
+    "url": "/beatmaker/static/media/wood.eb93a08a.jpg"
+  }
+]);
