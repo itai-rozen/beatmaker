dc73813f022b53246cb48878b08d37b5f1d91a24
itai rozen <itai.rozen@mail.huji.ac.il>
2020-11-08 22:07:55 +0200

    ,

--- app.js
+++ app.js
@@ -3,19 +3,7 @@ app = express()
 const Preset = require('./models/preset')
 
 app.use(express.urlencoded({extends: true}))
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-// app.get('/', (req,res) => {
-//     res.render('index')
-// })
-
-  app.get('/api', (req,res)=> {
+app.get('/api', (req,res)=> {
     Preset.find()
     .sort({createdAt: -1})
     .then(result => res.json(result))
@@ -31,7 +19,6 @@ app.post('/api', (req,res)=> {
     })
     .catch(err => console.error(err)) 
 })
-  
 
 
 const mongoose  = require('mongoose')
@@ -40,10 +27,11 @@ require('dotenv').config()
 app.set('view engine', 'ejs')
 
 
-const connect = 'mongodb+srv://itai_rozen:1234@cluster0.sihrb.mongodb.net/preset-list?retryWrites=true&w=majority'
+const connect = process.env.MONGODB_URI
 const PORT = process.env.PORT || 3001
 mongoose.connect(connect,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(result => app.listen(PORT, ()=> {
     console.log('listning on port ',PORT)
 })
 )
+
