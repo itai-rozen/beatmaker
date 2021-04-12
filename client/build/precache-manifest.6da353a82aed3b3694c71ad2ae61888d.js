677d84447f15e99ec0c25b45da7c5a712dcd55ab
itai rozen <itai.rozen@mail.huji.ac.il>
2021-04-12 14:22:21 +0300

    ..

---
+++ client/build/precache-manifest.6da353a82aed3b3694c71ad2ae61888d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6453cf71e601c9f09c87c14d6941fbf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e80cffa50e2dac88bee",
+    "url": "/static/css/main.26ba625c.chunk.css"
+  },
+  {
+    "revision": "2c38c182b23eead1d6b0",
+    "url": "/static/js/2.16738a3d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.16738a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffee5bfd806b7bba91a",
+    "url": "/static/js/3.34ffda33.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.34ffda33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e80cffa50e2dac88bee",
+    "url": "/static/js/main.8fb1828e.chunk.js"
+  },
+  {
+    "revision": "2ecfe9633f20e8f519b1",
+    "url": "/static/js/runtime-main.b5a8285e.js"
+  },
+  {
+    "revision": "0f3080b9498ab5bc14e6c13570313ba4",
+    "url": "/static/media/2.0f3080b9.jpg"
+  },
+  {
+    "revision": "3f94a69eeceb496fd0fc9031e137af84",
+    "url": "/static/media/all-instruments.3f94a69e.mp3"
+  },
+  {
+    "revision": "4c92d2fd76b642deb8c448bf2269ea08",
+    "url": "/static/media/punk kid.4c92d2fd.ttf"
+  },
+  {
+    "revision": "eb93a08ade6de32e954ee135cd390b4f",
+    "url": "/static/media/wood.eb93a08a.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beatmaker/precache-manifest.526d228e6981e17c5807134ef02926d7.js"
+  "/precache-manifest.6da353a82aed3b3694c71ad2ae61888d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beatmaker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
