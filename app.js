6aefe232a2db90c270e5a33b2f023108b89ba39f
itai rozen <itai.rozen@mail.huji.ac.il>
2023-01-31 21:51:17 +0200

    static path!!

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/public')))
 // if (process.env.NODE_ENV === 'production') {
   // app.get('/.*/',(req,res) => res.sendFile(path.join(__dirname,'client/build/index.html')))
 // }
