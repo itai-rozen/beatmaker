26c3f239d009b78bda851d220b1e049dad77bbc5
itai rozen <itai.rozen@mail.huji.ac.il>
2023-01-31 20:29:19 +0200

    added cord=s & expres.json & changed static path

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express'),
 app = express()
+const cors = require('cors')
+require('dotenv').config()
 const Preset = require('./models/preset')
-
+app.use(cors())
+app.use(express.json())
 app.use(express.urlencoded({extends: true}))
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname + '/public/'))
-  app.get('/.*/',(req,res) => res.sendFile(__dirname + '/public/index.html'))
+  app.use(express.static(__dirname + '/client/public'))
+  app.get('/.*/',(req,res) => res.sendFile(__dirname + '/client/public/index.html'))
 }
 app.get('/api', (req,res)=> {
     Preset.find()
@@ -26,7 +29,7 @@ app.post('/api', (req,res)=> {
 
 const mongoose  = require('mongoose')
 
-require('dotenv').config()
+
 
 
 const connect = process.env.MONGODB_URI
