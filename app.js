587cbacfe901965e6e55fbe798dd1e03308c550c
itai rozen <itai.rozen@mail.huji.ac.il>
2023-01-31 19:39:01 +0200

    changed static path again

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Preset = require('./models/preset')
 app.use(express.urlencoded({extends: true}))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'))
-  app.get('/.*/',(req,res) => res.sendFile(__dirname + '../client/public/index.html'))
+  app.get('/.*/',(req,res) => res.sendFile(__dirname + 'client/public/index.html'))
 }
 app.get('/api', (req,res)=> {
     Preset.find()
