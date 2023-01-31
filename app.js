8e921b8d5a2548265674091c362fb607d1a72442
itai rozen <itai.rozen@mail.huji.ac.il>
2023-01-31 20:48:01 +0200

    static path again!!!

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express'),
 app = express()
 const cors = require('cors')
 require('dotenv').config()
-const Preset = require('./models/preset')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname + '/client/public'))
-  app.get('/.*/',(req,res) => res.sendFile(__dirname + '/client/public/index.html'))
+  app.use(express.static(__dirname + '/public'))
+  app.get('/.*/',(req,res) => res.sendFile(__dirname + '/public/index.html'))
 }
+const Preset = require('./models/preset')
 app.get('/api', (req,res)=> {
     Preset.find()
     .sort({createdAt: -1})
